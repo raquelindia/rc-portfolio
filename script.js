4d6494a066ee279d0301195af75e80dacc62cda4
Raquel Cruz <raquelicruz94@gmail.com>
2024-05-10 18:01:56 -0400

    added projects'

--- script.js
+++ script.js
@@ -4,19 +4,33 @@ app.controller('appCtrl', function($scope, $filter, $timeout, $cookies){
 
 $scope.projectsCarousel = [
     {
-        image: "./projects-files/images/ai-assistant-app.png",
+        image: "./projects-files/images/ai-project-conversation.png",
         title: "AI Assistant",
         caption: `I used the ChatGpt API to create this.
          I didn't fine tune it for anything and the only instructions 
          I gave it were to be helpful and speak in a formal tone because I wanted it to 
          mostly be like the regular ChatGPT but with themes.
+        `,
+        stack: `
+        AngularJS,
+        Tailwind,
+        Render,
+        NodeJS,
+        Google Material Icons,
+        ChatGpt API,
         `
     },
     {
         image: "./projects-files/images/mobile-mars-weather-app.png",
         title: "Mars Weather App",
         caption: `For this project I tried to create something that looks similar to the Apple 
-        Weather App. I thought the idea of a weather app would be cool but I also am really interested in space. I used NASAs Curiosity Rover API to get the data I needed for it.`
+        Weather App. I thought the idea of a weather app would be cool but I also am really interested in space. I used NASAs Curiosity Rover API to get the data I needed for it.`,
+        stack: `
+        AngularJS,
+        Tailwind,
+        Render,
+        Google Material Icons
+        `
     }
 ];
 
